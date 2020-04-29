a85f149958cffe997c6db0da478fb715ffebc5dd
Alfurquan Zahedi <alfurquan.z@aqinsights.com>
2020-04-29 17:21:45 +0530

    Initial Commit

---
+++ Skinet/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Skinet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly SkinetDbContext context;
+
+        public ProductsController(SkinetDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task <IActionResult> GetProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+            return Ok(product);
+        }
+        
+    }
+}
